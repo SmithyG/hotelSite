e8cadeb0e355ee611797cc29dd7e550c392cfed8
George Smith <smithy_g@live.com>
2017-11-17 02:56:12 +0000

    Pages have been created. Flask framework implemented. Reviews page has form however needs to record the date when a comment is added

--- exampleScript.py
+++ exampleScript.py
@@ -52,4 +52,3 @@ def addSkill():
     
 if __name__ == '__main__':
     app.run(debug = True)
-    
--- script.py
+++ script.py
@@ -4,6 +4,58 @@ import csv
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/attractions')
+def attractions():
+    return render_template('attractions.html')
+
+
+@app.route('/bookings')
+def bookings():
+    return render_template('bookings.html')
+
+
+@app.route('/reviews')
+def reviews():
+    with open('static\\reviews.csv', 'r') as inFile:
+        reader = csv.reader(inFile)
+        reviewList = [row for row in reader]
+    return render_template('reviews.html', reviewList=reviewList)
+
+
+def readFile(aFile):
+    with open(aFile, 'r') as inFile:
+        reader = csv.reader(inFile)
+        reviewList = [row for row in reader]
+    return reviewList
+
+
+def writeFile(aList, aFile):
+    with open(aFile, 'w',) as outFile:
+        writer = csv.writer(outFile)
+        writer.writerows(aList)
+    return
+
+
+@app.route('/addReview', methods=['POST'])
+def addReview():
+    reviewFile = 'static\\reviews.csv'
+    reviewList = readFile(reviewFile)
+
+    name = request.form[('name')]
+    comment = request.form[('comment')]
+
+    newReview = [name, comment]
+    reviewList.append(newReview)
+
+    writeFile(reviewList, reviewFile)
+    return render_template('reviews.html', reviewList=reviewList)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
